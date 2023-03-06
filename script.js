5e2fc5384ae36f54f41b37c8239897bb49635880
SMeyer-8 <sarah.blue.meyer@gmail.com>
2023-03-06 11:33:17 -0600

    script js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // Your code here
+
+
+const table = document.getElementsByTagName('table')[0];
+
+function makeRow(event){
+    const row = document.createElement('tr')
+    for (let i = 0; i < 20; i++) {
+        const td = document.createElement('td')
+        row.appendChild(td)
+    }
+    table.appendChild(row)
+}
+
+makeRow()
+makeRow()
+
+const addRowButton = document.getElementById('add-row')
+addRowButton.addEventListener('click', makeRow);
+
+table.addEventListener('click', colorize);
+function colorize(event){
+    
+    tdCell = event.target
+    if (tdCell.className.length){
+        tdCell.className = ""
+     
+    }
+    else{
+        tdCell.className = 'red'
+    }
+   
+  
+    
+    console.log(event);
+}
